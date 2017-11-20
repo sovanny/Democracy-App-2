15f973429f6f5926442fab7807afbda51f9cdbaa
sovanny <sovhu774@student.liu.se>
2017-11-20 16:41:08 +0900

    new database

---
+++ js/main.js
@@ -0,0 +1,258 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAlgtMKytcxjYYTxjIGiDgUEUm5yVOf3X0",
+    authDomain: "democracy-app-2.firebaseapp.com",
+    databaseURL: "https://democracy-app-2.firebaseio.com",
+    projectId: "democracy-app-2",
+    storageBucket: "democracy-app-2.appspot.com",
+    messagingSenderId: "895134222441"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// When site had loaded
+$( window ).on( "load", function() {
+
+    //Run main function
+    start()
+
+    function start() {
+
+        // A variable for the tabs
+        const $tabs = $('#bottom-navbar .tab')
+
+        // Run selectTab and loadCardContent once in order to show something upon loading
+        selectTab($tabs, $($tabs[0]))
+
+        var listOfUids = getUidList('feed_uids');
+        loadCardContent($('#feed-container'), listOfUids);
+
+
+        // When clicking one of the tabs
+        $tabs.on('click', function(e) {
+            // Change color of tab
+            selectTab($tabs, $(e.target));
+            // Display the right view
+            changeContent($(e.target));
+        })
+
+        // when clicking on checkmark
+        $('#feed-container').on('click', '.fa-check', function(e){
+            agree($(e.target));
+
+            var x = document.getElementById("snackbar")
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+
+            var uidList;
+            setTimeout(function() { uidList =  getUidList('feed_uids'); }, 3000);
+            setTimeout(function() { loadCardContent($('#feed-container'), uidList); }, 3000);
+
+
+    })
+        // when clicking on X
+        $('#feed-container').on('click', '.fa-times', function(e){
+            disAgree($(e.target));
+
+            var x = document.getElementById("snackbar")
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+            loadCardContent($('#feed-container'), getUidList('feed_uids'));
+        })
+        // when clicking on flag
+        $('#feed-container').on('click', '.fa-flag', function(e){
+            flagPost($(e.target));
+        })
+
+    }
+
+    // function definition
+    function selectTab($tabs, $tab) {
+        const selectedClass = 'selected';
+        $tabs.removeClass(selectedClass);
+        $tab.addClass(selectedClass);
+    }
+
+    function agree($button){
+        const agreedClass = 'agreed';
+        const notClickedClass = 'notClicked';
+        $button.removeClass(notClickedClass);
+        $button.addClass(agreedClass);
+
+        var cardUid = parseInt($button.closest("div .card").prop("id"));
+        database.ref('cards').once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    if (childSnapshot.val().UID == cardUid){
+
+                        var old_count = childSnapshot.val().agree_count;
+
+                        childSnapshot.ref.update({agree_count: old_count + 1});
+
+                    }
+                })});
+
+        database.ref('feed_uids').once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    if (childSnapshot.val().UID == cardUid){
+                        childSnapshot.ref.remove();
+
+                    }
+                })});
+
+        database.ref('my_votes_uids').push({"UID": cardUid});
+    }
+
+    function disAgree($button){
+        const disagreedClass = 'disagreed';
+        const notClickedClass = 'notClicked';
+        $button.removeClass(notClickedClass);
+        $button.addClass(disagreedClass);
+
+        var cardUid = parseInt($button.closest("div .card").prop("id"));
+        database.ref('cards').once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    if (childSnapshot.val().UID == cardUid){
+
+                        var old_count = childSnapshot.val().disagree_count;
+
+                        childSnapshot.ref.update({disagree_count: old_count + 1});
+
+                    }
+                })});
+
+        database.ref('feed_uids').once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    if (childSnapshot.val().UID == cardUid){
+                        childSnapshot.ref.remove();
+
+                    }
+                })});
+
+        database.ref('my_votes_uids').push({"UID": cardUid});
+
+    }
+
+    function flagPost($button){
+        const flaggedClass = 'flagged';
+        const notClickedClass = 'notClicked';
+        $button.removeClass(notClickedClass);
+        $button.addClass(flaggedClass);
+    }
+
+    // function definition
+    function changeContent($tab){
+
+        // What element to append view to
+        const $view = $('#feed-container')
+
+        //Check the ID of the tab clicked
+        if ($tab.attr('id') == 'home-feed-view'){
+            var listOfUids = getUidList('feed_uids');
+            loadCardContent($view, listOfUids);
+        }
+        else if ($tab.attr('id') == 'my-votes-view'){
+            var listOfUids = getUidList('my_votes_uids');
+            loadCardContent($view, listOfUids);
+        }
+        else if ($tab.attr('id') == 'my-posts-view') {
+
+            var listOfUids = getUidList('my_posts_uids');
+            loadCardContent($view, listOfUids);
+        }
+        else if ($tab.attr('id') == 'new-post-view'){
+            loadHtml($view, 'new post/new');
+        }
+    }
+
+    // This function is used for testing
+    // It loads an html file into the selected element
+    function loadHtml($view, fileName) {
+        $.ajax({
+            url: fileName + ".html",
+            success: function (data) {
+                $view.empty();
+                $view.append(data)
+            },
+            dataType: 'html'
+        });
+    }
+
+    // This is the actual function that we can use
+    function loadCardContent($view, uidList) {
+
+        // empty the current content
+        $view.empty();
+
+        var cardContent = database.ref('cards');
+
+
+        /* Checks for newly added cards
+        cardContent.on('child_added', function (snapshot) {
+            var data = snapshot.val();
+        });*/
+
+
+        cardContent.once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    card = childSnapshot.val()
+                    if (uidList.indexOf(card.UID) > -1){
+                        $view.append("<div class=\"card\"" + "id=\"" + card.UID + "\"" +">\n" +
+                            "        <p class=\"card-title\">\n" +
+                            card.title +
+                            "        </p>\n" +
+                            //"        <p class=\"card-content\">\n" +
+                            //card.text +
+                            //"        </p>\n" +
+                            "        <div class=\"footer-container\">\n" +
+                            "<div class=\"flag-container\">\n" +
+                            "            <i class=\"fa fa-flag notClicked\" aria-hidden=\"true\"></i>\n" +
+                            "        </div>\n" +
+                            "    <div class=\"date-container\">\n" +
+                            "        13 nov '17\n" +
+                            "    </div>\n" +
+                            "\n" +
+                            "        <div class=\"disagree-container\">\n" +
+                            "            <i class=\"fa fa-times notClicked\" aria-hidden=\"true\"></i>\n" +
+                            "<span class=\"disagree-count\">" + card.disagree_count + "</span>" +
+                            "        </div>\n" +
+                            "        <div class=\"agree-container\">\n" +
+                            "            <i class=\"fa fa-check notClicked\" aria-hidden=\"true\"></i>\n" +
+                            "<span class=\"agree-count\">" + card.agree_count  + "</span>" +
+                            "        </div>" +
+                            "        </div>\n" +
+                            "    </div>\n" +
+                            "        </div>\n" +
+                            "    </div>");
+                    }
+                })});
+
+    }
+
+    function getUidList(nameOfList){
+        var listOfUids = []
+        var myPostsListRef = database.ref(nameOfList);
+
+        myPostsListRef.once("value")
+            .then(function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    listOfUids.push(childSnapshot.val().UID)
+                })
+            })
+        return listOfUids;
+    }
+
+});
+
+
+
+
+
---
+++ new post/new.js
@@ -0,0 +1,174 @@
+//firebase();
+//  var config = {
+//      apiKey: "AIzaSyD8sQuV_OsNSqbHgVnQFKn6d2YCDGlfJxc",
+//      databaseURL: "https://democracy-app-kaist.firebaseio.com/"
+// }
+//
+// firebase.initializeApp(config);
+// var database = firebase.database()
+
+
+var userRef = database.ref("users")
+var cardRef = database.ref("cards")
+var myPostRef = database.ref("my_posts_uids")
+
+var next_uidRef = database.ref("next_uid")
+//
+//
+// console.log('below')
+next_uidRef.once("value")
+    .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            next_uid = childSnapshot.val();
+        })})
+
+
+
+
+$(document).ready(function(){
+    $('#feed-container').on('click', '#postBtn', function(e){
+        title = document.getElementById("title").value;
+        details = document.getElementById("details").value;
+        url = document.getElementById("url").value;
+        date = document.getElementById("date").value;
+        // date.placeholder = Date.now();
+        // if user didn't type date, it will be set to current date
+        if (date == "") {
+            date = Date.now();
+        }
+        cardRef.push({
+            UID: next_uid,
+            title: title,
+            text: details,
+            agree_count: 0,
+            disagree_count: 0,
+            time_stamp: date,
+            media_url: url,
+            flag_status: "noflag",
+        });
+
+        // also add this UIDs to my_posts
+        myPostRef.push({
+            UID: next_uid,
+        });
+        // don't add it to my_votes
+
+        next_uidRef.once("value")
+            .then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    snapshot.ref.update({ next_uid: next_uid + 1 })
+                    next_uid += 1;
+                })})
+        //window.location.href = "../index.html";
+    //     $.ajax({
+    //         url: "feed.html",
+    //         success: function (data) {
+    //             $('#feed-container2').empty();
+    //             $('#feed-container2').append(data)
+    //         },
+    //         dataType: 'html'
+    //     });
+    // });
+        const $view = $('#feed-container')
+        var listOfUids = getUidList("my_posts_uids")
+
+        loadCardContent($view, listOfUids);
+        // A variable for the tabs
+        const $tabs = $('#bottom-navbar .tab')
+
+        // Run selectTab and loadCardContent once in order to show something upon loading
+        selectTab($tabs, $($tabs[2]))
+    })
+
+});
+
+function selectTab($tabs, $tab) {
+    const selectedClass = 'selected';
+    $tabs.removeClass(selectedClass);
+    $tab.addClass(selectedClass);
+}
+
+function getUidList(nameOfList){
+    var listOfUids = []
+    var myPostsListRef = database.ref(nameOfList);
+
+    myPostsListRef.once("value")
+        .then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                console.log(childSnapshot.val().UID)
+                listOfUids.push(childSnapshot.val().UID)
+            })
+        })
+    return listOfUids;
+}
+
+// // This is the actual function that we can use
+
+function loadCardContent($view, uidList) {
+
+    // empty the current content
+    $view.empty();
+
+    var cardContent = database.ref('cards');
+    console.log(cardContent);
+
+    /* Checks for newly added cards
+    cardContent.on('child_added', function (snapshot) {
+        var data = snapshot.val();
+        console.log(data)
+        console.log('jag kör')
+
+    });*/
+
+
+    cardContent.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                card = childSnapshot.val()
+                if (uidList.indexOf(card.UID) > -1){
+                    $view.append("<div class=\"card\"" + "id=\"" + card.UID + "\"" +">\n" +
+                        "        <p class=\"card-title\">\n" +
+                        card.title +
+                        "        </p>\n" +
+                        //"        <p class=\"card-content\">\n" +
+                        //card.text +
+                        //"        </p>\n" +
+                        "        <div class=\"footer-container\">\n" +
+                        "<div class=\"flag-container\">\n" +
+                        "            <i class=\"fa fa-flag notClicked\" aria-hidden=\"true\"></i>\n" +
+                        "        </div>\n" +
+                        "    <div class=\"date-container\">\n" +
+                        "        13 nov '17\n" +
+                        "    </div>\n" +
+                        "\n" +
+                        "        <div class=\"disagree-container\">\n" +
+                        "            <i class=\"fa fa-times notClicked\" aria-hidden=\"true\"></i>\n" +
+                        "<span class=\"disagree-count\">" + card.disagree_count + "</span>" +
+                        "        </div>\n" +
+                        "        <div class=\"agree-container\">\n" +
+                        "            <i class=\"fa fa-check notClicked\" aria-hidden=\"true\"></i>\n" +
+                        "<span class=\"agree-count\">" + card.agree_count  + "</span>" +
+                        "        </div>" +
+                        "        </div>\n" +
+                        "    </div>\n" +
+                        "        </div>\n" +
+                        "    </div>");
+                }
+            })});
+
+}
+
+
+
+
+
+// Card attributes:
+//     UID
+//     Title
+//     Descriptive text
+//     Flag status
+//     Agree count
+//     Disagree count
+//     Time stamp
+//     Media url
+
