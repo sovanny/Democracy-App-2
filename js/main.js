6e8ef6ba7e08779687cc0554d4ca664a0647d1ec
OLGKAD <olzhas.kadyrakunov@gmail.com>
2017-11-27 22:23:11 +0900

    added a 'TA' user. created a README file

--- js/main.js
+++ js/main.js
@@ -602,3 +602,10 @@ $(window).on("load", function () {
 //     my_votes_uids: [{uid: 0, vote: 0}, {uid: 4, vote: -2}, {uid: 5, vote: -1}]
 // });
 
+// userRef.push({
+//     ID: 0,
+//     password: 'TA',
+//     feed_uids: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+//     my_posts_uids: [-1],
+//     my_votes_uids: [{uid: 0, vote: 0}]
+// });
--- js/temp.js
+++ js/temp.js
@@ -1,8 +1,17 @@
-cardRef.once("value")
-    .then(function(snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            if (childSnapshot.val().UID > 9) {
-                childSnapshot.ref.remove()
-            }
-        })
-        })
+// cardRef.once("value")
+//     .then(function(snapshot) {
+//         snapshot.forEach(function (childSnapshot) {
+//             if (childSnapshot.val().UID > 9) {
+//                 childSnapshot.ref.remove()
+//             }
+//         })
+//         })
+
+
+// userRef.push({
+//     ID: 00,
+//     password: 'TA',
+//     feed_uids: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+//     my_posts_uids: [-1],
+//     my_votes_uids: [{uid: 0, vote: 0}]
+// });
