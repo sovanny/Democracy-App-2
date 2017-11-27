6c083a85fd0f1788ae0c4a15b26b9b40a167e356
OLGKAD <olzhas.kadyrakunov@gmail.com>
2017-11-27 16:27:09 +0900

    prevent "agree" and "disagree" buttons from being pressed simultaneously

--- js/main.js
+++ js/main.js
@@ -245,28 +245,48 @@ $(window).on("load", function () {
                 .then(function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
                         if (childSnapshot.val().UID == cardUid) {
-
                             var old_agree_count = childSnapshot.val().agree_count
-                            ///
-                           // document.getElementById("disagreeBtn" + cardUid).classList.remove("disagreed")
-                           // document.getElementById("disagreeBtn" + cardUid).addClass('noClicked')
-                            ///
                             var old_disagree_count = childSnapshot.val().disagree_count
+                            remove_feed_uid(cardUid)
                             if (old_agree_count == 99) {
                                 $("#" + cardUid).addClass('in-progress')
                             }
                             document.getElementById("agree-count" + cardUid).innerHTML = old_agree_count + 1
                             childSnapshot.ref.update({agree_count: old_agree_count + 1});
-                            // in case it was there with a 'disagree' status
-                            // if ($button.hasClass('disagreed')) {
-                            //     remove_my_vote_uid(cardUid)
-                            //     childSnapshot.ref.update({disagree_count: old_disagree_count - 1});
-                            // }
+                            // if post was priorly downvoted
+                            if (document.getElementById("disagreeBtn" + cardUid).classList.contains('disagreed')) {
+                                // visually "uncheck" disagreed button
+                                document.getElementById("disagreeBtn" + cardUid).classList.remove("disagreed")
+                                $("#disagreeBtn" + cardUid).addClass('noClicked')
+                                document.getElementById("disagree-count" + cardUid).innerHTML = old_disagree_count - 1
+                                // update count in firebase
+                                childSnapshot.ref.update({disagree_count: old_disagree_count - 1})
+                                // update status in firebase. Don't have to delete the record. Just change its vote status.
+                                // don't use add_my_vote_uid. Just update vote status. Use it in else cause. This will work smoothly
+                                userRef.once("value")
+                                    .then(function(snapshot) {
+                                        snapshot.forEach(function(childSnapshot) {
+                                            if (childSnapshot.val().ID == currentUser) {
+                                                my_votes_uids_new = childSnapshot.val().my_votes_uids
+                                                // checks if my_vote_uid is in my_votes_uids, and if it is, - remove it
+                                                for (i = 0; i < my_votes_uids_new.length; i++) {
+                                                    if (cardUid == my_votes_uids_new[i].uid) {
+                                                        my_votes_uids_new[i].vote = -1
+                                                        childSnapshot.ref.update({my_votes_uids: my_votes_uids_new})
+                                                    }
+                                                }
+                                            }
+                                        })
+                                    })
+                            } else {
+                                add_my_vote_uid(cardUid, -1)
+                            }
+
+
+
                         }
                     })
                 });
-            remove_feed_uid(cardUid)
-            add_my_vote_uid(cardUid, -1)
         }
 
     }
@@ -299,20 +319,50 @@ $(window).on("load", function () {
 
 
             var cardUid = parseInt($button.closest("div .card").prop("id"));
-            // update disagree_count
+            // update agree_count
             database.ref('cards').once("value")
                 .then(function (snapshot) {
                     snapshot.forEach(function (childSnapshot) {
                         if (childSnapshot.val().UID == cardUid) {
+                            var old_agree_count = childSnapshot.val().agree_count
+                            var old_disagree_count = childSnapshot.val().disagree_count
+                            remove_feed_uid(cardUid)
+                            document.getElementById("disagree-count" + cardUid).innerHTML = old_disagree_count + 1
+                            childSnapshot.ref.update({disagree_count: old_disagree_count + 1});
+                            // if post was priorly upvoted
+                            if (document.getElementById("agreeBtn" + cardUid).classList.contains('agreed')) {
+                                // visually "uncheck" agreed button
+                                document.getElementById("agreeBtn" + cardUid).classList.remove("agreed")
+                                $("#agreeBtn" + cardUid).addClass('noClicked')
+                                document.getElementById("agree-count" + cardUid).innerHTML = old_agree_count - 1
+                                // update count in firebase
+                                childSnapshot.ref.update({agree_count: old_agree_count - 1})
+                                // update status in firebase. Don't have to delete the record. Just change its vote status.
+                                // don't use add_my_vote_uid. Just update vote status. Use it in else cause. This will work smoothly
+                                userRef.once("value")
+                                    .then(function(snapshot) {
+                                        snapshot.forEach(function(childSnapshot) {
+                                            if (childSnapshot.val().ID == currentUser) {
+                                                my_votes_uids_new = childSnapshot.val().my_votes_uids
+                                                // checks if my_vote_uid is in my_votes_uids, and if it is, - remove it
+                                                for (i = 0; i < my_votes_uids_new.length; i++) {
+                                                    if (cardUid == my_votes_uids_new[i].uid) {
+                                                        my_votes_uids_new[i].vote = -2
+                                                        childSnapshot.ref.update({my_votes_uids: my_votes_uids_new})
+                                                    }
+                                                }
+                                            }
+                                        })
+                                    })
+                            } else {
+                                add_my_vote_uid(cardUid, -2)
+                            }
 
-                            var old_count = childSnapshot.val().disagree_count;
-                            document.getElementById("disagree-count" + cardUid).innerHTML = old_count + 1
-                            childSnapshot.ref.update({disagree_count: old_count + 1});
+
+
                         }
                     })
                 });
-            remove_feed_uid(cardUid)
-            add_my_vote_uid(cardUid, -2)
         }
 
     }
